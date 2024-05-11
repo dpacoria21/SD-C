40e50ab4c44d4dd0bada77a0a9e4f9a6beb990e6
hgalvezq <hgalvezq@unsa.edu.pe>
2024-05-10 23:51:12 -0500

    Algoritmo de Lamport corregido

--- lab02/Algoritmo_de_Lamport/LamportClock.java
+++ lab02/Algoritmo_de_Lamport/LamportClock.java
@@ -24,35 +24,36 @@ public class LamportClock {
     }
 
     public static void main(String[] args) {
-            List < Thread > threads = new ArrayList < > ();
-            LamportClock clock = new LamportClock();
-            for(int i = 0; i < 5; i++) {
-                Thread thread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            int time = clock.tick();
-                            System.out.println("Thread " + Thread.currentThread().getId() + " created
-                                event with Lamport time " + time);
-                                try {
-                                    Thread.sleep((long)(Math.random() * 1000));
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                                int receivedTime = clock.tick(); System.out.println("Thread " + Thread.currentThread().getId() + " received
-                                    event with Lamport time " + receivedTime);
-                                    clock.update(receivedTime);
-                                }
-                            });
-                        threads.add(thread);
-                        thread.start();
+        List<Thread> threads = new ArrayList<>();
+        LamportClock clock = new LamportClock();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int time = clock.tick();
+                    System.out.println(
+                            "Thread " + Thread.currentThread().getId() + " created event with Lamport time " + time);
+                    try {
+                        Thread.sleep((long) (Math.random() * 1000));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    for(Thread thread: threads) {
-                        try {
-                            thread.join();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    System.out.println("Final Lamport time: " + clock.getTime());
+                    int receivedTime = clock.tick();
+                    System.out.println("Thread " + Thread.currentThread().getId() + " received event with Lamport time "
+                            + receivedTime);
+                    clock.update(receivedTime);
                 }
+            });
+            threads.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Final Lamport time: " + clock.getTime());
+    }
 }
