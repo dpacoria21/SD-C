46b6492066bd4f8fb0849d7bd644a452f33d9447
fcardenasm <fcardenasm@unsa.edu.pe>
2024-05-11 01:45:51 -0500

    Add: interfaz grafica

--- lab02/Algoritmo_de_Berkeley/Berkeley.py
+++ lab02/Algoritmo_de_Berkeley/Berkeley.py
@@ -1,24 +1,25 @@
+from tkinter import *
 import time
 
 def getHoraCliente1():
     horaUTC = time.localtime()
     desface = 20
     segundos = int(horaUTC[3])*3600 + int(horaUTC[4])*60 + int(horaUTC[5]) + desface * 60
-    return list(time.gmtime(segundos))[3:6]
+    return list(time.gmtime(segundos))
 
 def getHoraCliente2():
     horaUTC = time.localtime()
     desface = -10
     segundos = int(horaUTC[3])*3600 + int(horaUTC[4])*60 + int(horaUTC[5]) + desface * 60
-    return list(time.gmtime(segundos))[3:6]
+    return list(time.gmtime(segundos))
 
 def getHoraServer():
     horaUTC = time.localtime()
-    return list(horaUTC)[3:6]
+    return list(horaUTC)
 
 def calcularDiferencias(horaCliente, horaServer):
-    segundosCliente = int(horaCliente[0])*3600 + int(horaCliente[1])*60 + int(horaCliente[2])
-    segundosServer = int(horaServer[0])*3600 + int(horaServer[1])*60 + int(horaServer[2])
+    segundosCliente = int(horaCliente[3])*3600 + int(horaCliente[4])*60 + int(horaCliente[5])
+    segundosServer = int(horaServer[3])*3600 + int(horaServer[4])*60 + int(horaServer[5])
     return segundosCliente - segundosServer
 
 def calcularHoras(diferencias, *horas):
@@ -26,23 +27,34 @@ def calcularHoras(diferencias, *horas):
     nuevasDiferencias = [promedio - diferencia for diferencia in diferencias]
     nuevasHoras = []
     for i in range(len(horas)):
-        segundos = int(horas[i][0])*3600 + int(horas[i][1])*60 + int(horas[i][2]) + nuevasDiferencias[i]
-        nuevasHoras.append(list(time.gmtime(segundos))[3:6])
+        segundos = int(horas[i][3])*3600 + int(horas[i][4])*60 + int(horas[i][5]) + nuevasDiferencias[i]
+        nuevasHoras.append(list(time.gmtime(segundos)))
     return nuevasHoras
 
 def formatoHora(hora):
-    return "{:02d}:{:02d}:{:02d}".format(hora[0], hora[1], hora[2])
+    return "{:02d}:{:02d}:{:02d}:{:03d}".format(hora[3], hora[4], hora[5], hora[6]*10)
 
 def obtenerHoraCliente():
     nombres = ["Servidor", "Cliente 1", "Cliente 2"]
     horas = [getHoraServer(), getHoraCliente1(), getHoraCliente2()]
     diferencias = [calcularDiferencias(hora, horas[0]) for hora in horas]
     nuevasHoras = calcularHoras(diferencias, *horas)
-    print("Horas originales:")
     for i in range(len(horas)):
-        print(nombres[i] + ": " + formatoHora(horas[i]))
-    print("Nuevas horas:")
+        text.insert(END, nombres[i] + " (antes): " + formatoHora(horas[i]) + "\n")
+    text.insert(END, "--------------------------------------------\n")
     for i in range(len(nuevasHoras)):
-        print(nombres[i] + ": " + formatoHora(nuevasHoras[i]))
+        text.insert(END, nombres[i] + " (después): " + formatoHora(nuevasHoras[i]) + "\n")
+    text.insert(END, "========================\n")
+    text.see(END)
+    root.after(5000, obtenerHoraCliente)  
 
+root = Tk()
+root.geometry("300x450")
+text = Text(root, font=('times', 12, 'bold'), bg='white')
+text.pack(fill=BOTH, expand=YES)
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT, fill=Y)
+text.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=text.yview)
 obtenerHoraCliente()
+root.mainloop()
