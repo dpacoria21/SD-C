4e6d70bbc8f64e4e440a5ba067b1773f8d2ff9e5
hgalvezq <hgalvezq@unsa.edu.pe>
2024-05-10 23:23:59 -0500

    Creación del socket del servidor

--- lab02/Algoritmo_de_Cristian/ClockServer.java
+++ lab02/Algoritmo_de_Cristian/ClockServer.java
@@ -4,7 +4,16 @@ import java.net.*;
 import java.io.*;
 
 public class ClockServer {
+    private static final int SERVER_PORT = 8000;
+
     public static void main(String[] args) {
-        // Implementación pendiente
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            System.out.println("Servidor de reloj iniciado en el puerto " + SERVER_PORT);
+
+            // Aceptar conexiones y enviar la hora (implementación pendiente)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
