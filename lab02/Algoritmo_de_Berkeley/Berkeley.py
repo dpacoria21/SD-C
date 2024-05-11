dacc2382bcfc3e4586b8dc3824fed91cd442b9a6
fcardenasm <fcardenasm@unsa.edu.pe>
2024-05-11 00:31:25 -0500

    Add: funciones que obtienen la hora de un cliente y simulan un desface te tiempo en minutos

---
+++ lab02/Algoritmo_de_Berkeley/Berkeley.py
@@ -0,0 +1,13 @@
+import time
+
+def getHoraCliente1():
+    horaUTC = time.localtime()
+    desface = 20
+    segundos = int(horaUTC[3])*3600 + int(horaUTC[4])*60 + int(horaUTC[5]) + desface * 60
+    return list(time.gmtime(segundos))[3:6]
+
+def getHoraCliente2():
+    horaUTC = time.localtime()
+    desface = -10
+    segundos = int(horaUTC[3])*3600 + int(horaUTC[4])*60 + int(horaUTC[5]) + desface * 60
+    return list(time.gmtime(segundos))[3:6]
