a992f9e3c1c9a737bc5a76dbfbd5197ee4ec3f56
hgalvezq <hgalvezq@unsa.edu.pe>
2024-05-10 23:24:53 -0500

    Aceptar conexiones y enviar la hora

--- lab02/Algoritmo_de_Cristian/ClockServer.java
+++ lab02/Algoritmo_de_Cristian/ClockServer.java
@@ -2,6 +2,7 @@ package lab02.Algoritmo_de_Cristian;
 
 import java.net.*;
 import java.io.*;
+import java.util.Date;
 
 public class ClockServer {
     private static final int SERVER_PORT = 8000;
@@ -11,7 +12,22 @@ public class ClockServer {
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             System.out.println("Servidor de reloj iniciado en el puerto " + SERVER_PORT);
 
-            // Aceptar conexiones y enviar la hora (implementación pendiente)
+            while (true) {
+                System.out.println("Esperando conexiones...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
+
+                Date now = new Date();
+                String timeStr = String.valueOf(now.getTime());
+
+                OutputStream os = clientSocket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+                bw.write(timeStr + "\n");
+                bw.flush();
+
+                clientSocket.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
