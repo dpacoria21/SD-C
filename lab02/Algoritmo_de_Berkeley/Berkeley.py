b5a5e73ab6ca95043331e5715aae396e127a238d
fcardenasm <fcardenasm@unsa.edu.pe>
2024-05-11 00:33:53 -0500

    Add: funcion que calcula las nuevas horas para el cliente y para el servidor

--- lab02/Algoritmo_de_Berkeley/Berkeley.py
+++ lab02/Algoritmo_de_Berkeley/Berkeley.py
@@ -20,3 +20,12 @@ def calcularDiferencias(horaCliente, horaServer):
     segundosCliente = int(horaCliente[0])*3600 + int(horaCliente[1])*60 + int(horaCliente[2])
     segundosServer = int(horaServer[0])*3600 + int(horaServer[1])*60 + int(horaServer[2])
     return segundosCliente - segundosServer
+
+def calcularHoras(diferencias, *horas):
+    promedio = sum(diferencias) / len(diferencias)
+    nuevasDiferencias = [promedio - diferencia for diferencia in diferencias]
+    nuevasHoras = []
+    for i in range(len(horas)):
+        segundos = int(horas[i][0])*3600 + int(horas[i][1])*60 + int(horas[i][2]) + nuevasDiferencias[i]
+        nuevasHoras.append(list(time.gmtime(segundos))[3:6])
+    return nuevasHoras
