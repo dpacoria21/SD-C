019695ca521003dc2cd3035079f07e4d7e5528c6
fcardenasm <fcardenasm@unsa.edu.pe>
2024-05-11 00:32:04 -0500

    Add: funcion que obtiene la hora del servidor

--- lab02/Algoritmo_de_Berkeley/Berkeley.py
+++ lab02/Algoritmo_de_Berkeley/Berkeley.py
@@ -11,3 +11,7 @@ def getHoraCliente2():
     desface = -10
     segundos = int(horaUTC[3])*3600 + int(horaUTC[4])*60 + int(horaUTC[5]) + desface * 60
     return list(time.gmtime(segundos))[3:6]
+
+def getHoraServer():
+    horaUTC = time.localtime()
+    return list(horaUTC)[3:6]
